8f52f560c9eca6ab27161cbaae4add641a7d963f
Guilherme Dalla Rosa <gui.dallarosa@gmail.com>
2023-08-14 17:54:18 -0300

    Event listening

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
@@ -14,14 +15,22 @@ const Formulario = () => {
     "Inovação e Gestão"
   ]
 
+  const aoSalvar = evento => {
+    evento.preventDefault()
+    console.log("form foi submit")
+  }
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
         <CampoTexto label="Nome" placeholder="Digite seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
         <ListaSuspensa label="Time" itens={times}/>
+        <Botao>
+          Criar Card
+        </Botao>
       </form>
     </section>
   );
