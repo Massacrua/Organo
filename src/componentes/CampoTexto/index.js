a17b76e36542a45abd11d26ee68322f2ba5df3bc
Guilherme Dalla Rosa <gui.dallarosa@gmail.com>
2023-08-16 16:28:39 -0300

    Controlando estado dos inputs e select

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,13 @@
 import "./CampoTexto.css"
 
 const CampoTexto = props => {
+
+    const placeholderModificada = `${props.placeholder}...`
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={evento => props.aoAlterado(evento.target.value)} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
