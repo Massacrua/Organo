10b6bb5c3cb0eb5d2aa055903621dc67c484e90a
Guilherme Dalla Rosa <gui.dallarosa@gmail.com>
2023-08-22 17:00:46 -0300

    Componente Rodape

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,17 @@
+import "./Rodape.css"
+
+const Rodape = () => {
+    return (
+        <footer className="rodape">
+            <div className="contato">
+                <img src="/imagens/fb.png" alt="Facebook"/>
+                <img src="/imagens/tw.png" alt="Twitter"/>
+                <img src="/imagens/ig.png" alt="Instagram"/>
+            </div>
+            <img className="logo" src="/imagens/logo.png" alt="Logo Organo"/>
+            <p>Desenvolvido por Alura & Massacrua.</p>          
+        </footer>
+    )
+}
+
+export default Rodape
